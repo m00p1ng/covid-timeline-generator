42aa79e0e73621d60addf37f038accfe059c56f8
m00p1ng <mongkonchai4412@gmail.com>
2021-12-19 02:32:09 +0700

    [API] add entry and patient entity

---
+++ api/src/commons/enums/locationType.enum.ts
@@ -0,0 +1,6 @@
+export enum LocationType {
+  INDOOR = 'INDOOR',
+  OUTDOOR = 'OUTDOOR',
+  HOME = 'HOME',
+  TRAVELLING = 'TRAVELLING',
+}
---
+++ api/src/patients/patients.entity.ts
@@ -0,0 +1,60 @@
+import { 
+  BaseEntity, 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  OneToMany, 
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+import { Entry } from '../entries/entries.entity';
+
+@Entity()
+export class Patient extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ 
+    name: 'gender',
+    nullable: false,
+  })
+  gender: string;
+
+  @Column({ 
+    name: 'age',
+    nullable: false,
+  })
+  age: number;
+
+  @Column({ 
+    name: 'occupation',
+    nullable: false,
+  })
+  occupation: string;
+
+  @OneToMany(() => Entry, entry => entry.patient)
+  entries: Entry[];
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    nullable: false,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ 
+    name: 'deleted_at', 
+    type: 'timestamptz', 
+    nullable: true,
+  })
+  deletedAt: Date;
+}
