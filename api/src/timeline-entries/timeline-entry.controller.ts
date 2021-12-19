e96c424298fcd7f0c9d4b7ff18809d4f42c42565
m00p1ng <mongkonchai4412@gmail.com>
2021-12-19 12:28:48 +0700

    [API] remove unused files

--- api/src/timeline-entries/timeline-entry.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-import { TimelineEntryService } from './timeline-entry.service';
-
-@Controller('timeline-entries')
-export class TimelineEntryController {
-  constructor(private readonly timelineEntryService: TimelineEntryService) {}
-}
