aae0302093650250a591a72957570e8634bd7976
m00p1ng <mongkonchai4412@gmail.com>
2021-12-20 01:20:02 +0700

    [Frontend] add timeline entry component

--- frontend/components/TimelineEntryForm/TimelineEntryForm.tsx
+++ frontend/components/TimelineEntryForm/TimelineEntryForm.tsx
@@ -1,8 +1,48 @@
 const TimelineEntryForm = () => {
   return (
     <div className="timeline-entry-form-container">
-      TimelineEntryForm
-    </div >
+      <div className="grid grid-cols-12 gap-4 mb-6">
+        <div className="form-group col-span-8">
+          <label>From</label>
+          <input type="datetime-local" />
+        </div>
+
+        <div className="form-group col-span-4">
+          <label>Time</label>
+          <input
+            type="time"
+            placeholder="Your age"
+          />
+        </div>
+
+        <div className="form-group col-span-12">
+          <label>Detail</label>
+          <textarea rows={5} />
+        </div>
+
+        <div className="form-group col-span-4">
+          <label>Location Type</label>
+          <select>
+            <option value="INDOOR">Indoor</option>
+            <option value="OUTDOOR">Outdoor</option>
+            <option value="HOME">Home</option>
+            <option value="TRAVELLING">Travelling</option>
+          </select>
+        </div>
+
+        <div className="form-group col-span-8">
+          <label>Location</label>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="text-right">
+        <button
+          className="button primary-button"
+        >
+          + Add Entry
+        </button>
+      </div>
+    </div>
   )
 }
 
