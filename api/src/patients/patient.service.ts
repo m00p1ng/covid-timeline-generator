1a048353b1b5dcf235d571d4b8b5aa7eb9c2066f
m00p1ng <mongkonchai4412@gmail.com>
2021-12-19 11:20:29 +0700

    [API] add logic handle maximum patients

--- api/src/patients/patient.service.ts
+++ api/src/patients/patient.service.ts
@@ -1,18 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { UnprocessableEntityException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreatePatientDto } from './dto/create-patient.dto';
 
 import { Patient } from './patient.entity';
+import { CreatePatientDto } from './dto/create-patient.dto';
 
 @Injectable()
 export class PatientService {
   constructor(
     @InjectRepository(Patient)
     private readonly patientRepository: Repository<Patient>,
+    private readonly configService: ConfigService,
   ) {}
 
   public async createPatient(data: CreatePatientDto): Promise<Patient> {
+    const MAXIMUM_PATIENTS = parseInt(
+      this.configService.get<string>('MAXIMUM_PATIENTS'),
+      10,
+    );
+    const countPatients = await this.patientRepository.count();
+
+    if (countPatients >= MAXIMUM_PATIENTS) {
+      throw new UnprocessableEntityException(
+        'Current number of patients is reach the limit',
+      );
+    }
+
     const newPatient = new Patient();
     newPatient.gender = data.gender;
     newPatient.age = data.age;
