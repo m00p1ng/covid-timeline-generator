94e1a6745ff99ae9a363eec145bdf00049435b35
m00p1ng <mongkonchai4412@gmail.com>
2021-12-19 20:03:08 +0700

    [Frontend] add patient style

--- frontend/components/PatientTabs/PatientTabs.tsx
+++ frontend/components/PatientTabs/PatientTabs.tsx
@@ -1,6 +1,9 @@
+import classnames from 'classnames'
+
 import { Patient } from '../../interfaces/Patient';
 
 interface PatientTabItemProps {
+  index: number;
   patient: Patient;
   selected: boolean;
   setSelectedTab: (tab: string | number) => void;
@@ -12,23 +15,30 @@ interface PatientTabsProps {
   setSelectedTab: (tab: string | number) => void;
 }
 
-const PatientTabItem = ({ patient, selected, setSelectedTab }: PatientTabItemProps) => (
-  <div>
-    {patient.id}
+const PatientTabItem = ({ index, patient, selected, setSelectedTab }: PatientTabItemProps) => (
+  <div onClick={() => setSelectedTab(patient.id)} className="patient-tab-item">
+    <p className={classnames({ "primary-text": selected })}>Patient</p>
+    <h2 className={classnames({ "primary-text": selected })}>{index}</h2>
   </div>
 )
 
 const PatientTabs = ({ patients, selectedId, setSelectedTab }: PatientTabsProps) => {
   return (
-    <div>
-      {patients.map((patient) => (
+    <div className="flex flex-row mb-6">
+      {patients.map((patient, idx) => (
         <PatientTabItem
           key={patient.id}
           selected={selectedId === patient.id}
           setSelectedTab={setSelectedTab}
+          index={idx + 1}
           patient={patient}
         />
       ))}
+      <div onClick={() => setSelectedTab('Add')} className="patient-tab-item">
+        <h1 className={classnames({ "primary-text": selectedId === 'Add' })}>
+          +
+        </h1>
+      </div>
     </div>
   )
 }
