d4bc44706472869b7677e04c9d86d33db6b5fef5
m00p1ng <mongkonchai4412@gmail.com>
2021-12-19 21:00:18 +0700

    [Frontend] update patient info form

--- frontend/components/PatientInfo/PatientInfo.tsx
+++ frontend/components/PatientInfo/PatientInfo.tsx
@@ -2,16 +2,22 @@ const PatientInfo = () => (
   <div>
     <h3 className="primary-text">Patient Information</h3>
     <div className="patient-info-container mt-3">
-      <div>
-        <label>Gender</label>
-        <select>
-          <option value="MALE">Male</option>
-          <option value="FEMALE">Female</option>
-        </select>
-        <label>Age</label>
-        <input type="number" placeholder="Your age" />
-        <label>Occupation</label>
-        <input type="text" placeholder="Your Occupation" />
+      <div className="grid grid-cols-12 gap-4 mb-6">
+        <div className="form-group col-span-3">
+          <label>Gender</label>
+          <select>
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+          </select>
+        </div>
+        <div className="form-group col-span-3">
+          <label>Age</label>
+          <input type="number" placeholder="Your age" />
+        </div>
+        <div className="form-group col-span-6">
+          <label>Occupation</label>
+          <input type="text" placeholder="Your Occupation" />
+        </div>
       </div>
       <div className="text-right">
         <button className="button primary-button">+ Add Patient</button>
